3a417cd4d9ecec3c74c45abf942aead12f0bbfe6
Ankan Pal <ankanpal255@gmail.com>
2026-08-03 12:46:59 +0530

    Add simulated analytics/telemetry logger

---
+++ src/utils/analytics.js
@@ -0,0 +1,4 @@
+export function logAnalyticsEvent(actionLabel) {
+  // Simulated analytics ping
+  console.log(`[Analytics] User interacted with Wholesale Distributor Application: ${actionLabel}`)
+}
